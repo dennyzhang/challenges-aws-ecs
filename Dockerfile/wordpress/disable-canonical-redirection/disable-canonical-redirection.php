5428b65fdeb36e3e186f46dd5e5300f2239754be
dennyzhang <denny@dennyzhang.com>
2017-11-10 10:12:23 -0600

    update wp plugin

--- Dockerfile/wordpress/disable-canonical-redirection/disable-canonical-redirection.php
+++ Dockerfile/wordpress/disable-canonical-redirection/disable-canonical-redirection.php
@@ -1,2 +1,8 @@
 <?php
+/*
+Plugin Name: Disable Canonical URL Autocorrect Guessing
+Description: Disables Wordpress' URL autocorrection guessing feature. If you for example enter the URL http://www.myblog.com/proj you won't be redirected to http://www.myblog.com/project-2013 anymore.
+Author: Denny Zhang
+Version: 0.1
+*/
 remove_filter('template_redirect', 'redirect_canonical');
